a3029c723c1f1bd220e8788c78febcc84b4ae5eb
JoeyGordon <joey.gordon@gmail.com>
2017-09-11 18:31:32 -0700

    update build settings

--- src/main/java/com/bakkenbaeck/token/headless/TokenHeadlessClient.java
+++ src/main/java/com/bakkenbaeck/token/headless/TokenHeadlessClient.java
@@ -97,7 +97,7 @@ class TokenHeadlessClient {
                         flyway.setDataSource(pgconfig.getJdbcUrl(), pgconfig.getUsername(), pgconfig.getPassword());
                         flyway.setLocations("db/migration/psql");
                         flyway.setSchemas(schema);
-                        flyway.baselineOnMigrate = true;
+                        // flyway.baselineOnMigrate = true;
                         flyway.migrate();
                         schemas.remove(0);
                     }
